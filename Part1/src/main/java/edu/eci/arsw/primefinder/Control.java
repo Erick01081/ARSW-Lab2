83d415991caec171440274adcc49f4e9d366359a
Erick <erick.montero@mail.escuelaing.edu.co>
2024-08-24 19:47:00 -0500

    Punto 1 ya funcional

--- Part1/src/main/java/edu/eci/arsw/primefinder/Control.java
+++ Part1/src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.util.Scanner;
+
 public class Control extends Thread {
-    
     private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
+    private final static int MAXVALUE = 100000000;
+    private final static int TMILISECONDS = 120000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
-    
+    private final Object lock = new Object(); // Agregamos un lock para sincronización
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
+        for(i = 0; i < NTHREADS - 1; i++) {
             PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
             pft[i] = elem;
         }
         pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for(int i = 0; i < NTHREADS; i++) {
             pft[i].start();
         }
+
+        Scanner scanner = new Scanner(System.in);
+        long startTime = System.currentTimeMillis();
+
+        while (true) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - startTime >= TMILISECONDS) {
+                pauseThreads();
+                printPrimeCount();
+
+                System.out.println("Presione ENTER para continuar...");
+
+                synchronized (lock) {
+                    try {
+                        scanner.nextLine(); // Espera la entrada del usuario
+                        lock.notify(); // Notifica para continuar después de ENTER
+                    } catch (Exception e) {
+                        Thread.currentThread().interrupt();
+                        break;
+                    }
+                }
+
+                resumeThreads();
+                startTime = System.currentTimeMillis(); // Reiniciar el temporizador
+            }
+            synchronized (lock) {
+                try {
+                    lock.wait(1); // Espera corta usando el lock
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
     }
-    
+
+    private void pauseThreads() {
+        for (PrimeFinderThread thread : pft) {
+            thread.pauseThread();
+        }
+    }
+
+    private void resumeThreads() {
+        for (PrimeFinderThread thread : pft) {
+            thread.resumeThread();
+        }
+    }
+
+    private void printPrimeCount() {
+        int totalPrimes = 0;
+        for (PrimeFinderThread thread : pft) {
+            totalPrimes += thread.getPrimes().size();
+        }
+        System.out.println("Número de primos encontrados hasta ahora: " + totalPrimes);
+    }
 }
--- Part1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ Part1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,64 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
+    int a, b;
+    private List<Integer> primes;
+    private volatile boolean paused = false;
+    private final Object pauseLock = new Object();
 
-	
-	int a,b;
-	
-	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-                this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
-	}
+    public PrimeFinderThread(int a, int b) {
+        super();
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+    }
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
-                    primes.add(i);
-                    System.out.println(i);
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+            synchronized (pauseLock) {
+                while (paused) {
+                    try {
+                        pauseLock.wait(); // Espera a que se reanude
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
                 }
             }
-	}
-	
-	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println(i);
             }
-	    return ans;
-	}
+        }
+    }
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
+    boolean isPrime(int n) {
+        boolean ans;
+        if (n > 2) {
+            ans = n % 2 != 0;
+            for (int i = 3; ans && i * i <= n; i += 2) {
+                ans = n % i != 0;
+            }
+        } else {
+            ans = n == 2;
+        }
+        return ans;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public void pauseThread() {
+        paused = true;
+    }
+
+    public void resumeThread() {
+        synchronized (pauseLock) {
+            paused = false;
+            pauseLock.notify(); // Notifica para reanudar el hilo
+        }
+    }
 }
